59fb47c8670dbe0fbab522c30cc068efed456269
moisesVeSouza <mvs338409@hotmail.com>
2026-03-27 07:35:30 -0300

    feat: create routes, using styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <h1>Hello World</h1>
-  );
-}
+import GlobalStyle from './styles/global';
+import AppRoutes from './routes';
+
+const App: React.FC = () => (
+    <>
+        <GlobalStyle />
+        <BrowserRouter>
+            <AppRoutes />
+        </BrowserRouter>
+    </>
+);
 
 export default App;
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+const Dashboard: React.FC = () => {
+    return (
+        <>
+            <Title>Explore repositórios no Github</Title>
+        </>
+    );
+};
+
+export default Dashboard;
