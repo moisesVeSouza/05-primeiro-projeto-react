187159ab2edc8d464c40b3f17b5568d8a1aaff63
moisesVeSouza <mvs338409@hotmail.com>
2026-04-27 19:10:44 -0300

    feat: add repositories list

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,10 +1,53 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { Header, RepositoryInfo, Issues } from './styles';
+
+import logoSvg from '../../assets/logo.svg';
 
 const Repository: React.FC = () => {
     const { repository = '' } = useParams();
 
-    return <h1>Repository: {repository}</h1>;
+    return (
+        <>
+            <Header>
+                <img src={logoSvg} alt="Github Explorer" />
+                <Link to="/"> Voltar </Link>
+            </Header>
+
+            <RepositoryInfo>
+                <header>
+                    <img src={logoSvg} alt="Github Explorer" />
+                    <div>
+                        <strong>rocketseat/unform</strong>
+                        <p>descrição do reposotório</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>1808</strong>
+                        <span>Stars</span>
+                    </li>
+                    <li>
+                        <strong>48</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>67</strong>
+                        <span>Issues abertas</span>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+
+            <Issues>
+                <Link to={'assad'}>
+                    <div>
+                        <strong>asda</strong>
+                        <p>asada</p>
+                    </div>
+                </Link>
+            </Issues>
+        </>
+    );
 };
 
 export default Repository;
